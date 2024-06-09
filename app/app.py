eeb8962f09cb1685088b913d8822ef777f89ab35
yahya mo <yahyamohmuedpro99@gmail.com>
2024-06-09 04:20:29 +0300

    register the app bp's

--- app/app.py
+++ app/app.py
@@ -1,17 +1,13 @@
 from flask import Flask
-import pymongo
+from flask_jwt_extended import JWTManager
 # from flask_login import LoginManager
-
 app = Flask(__name__)
-
-# init extensions (e.g., Flask-Login if needed)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
 # Register blueprints from routes and controllers
-from routes import main_routes
-app.register_blueprint(main_routes.bp)
+from routes import main_routes,auth,admin_routes
+app.register_blueprint(main_routes.api_bp,prefix="/api")
+app.register_blueprint(auth.auth, url_prefix='/auth')
+app.register_blueprint(admin_routes.admin_bp, url_prefix='/admin')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
